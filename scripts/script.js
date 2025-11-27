1b249494163de8d02648c973315b6cbd99b440d6
NNaassttyya <maluhina557@mail.ru>
2025-11-27 12:11:59 +0500

    Готовый проект

--- scripts/script.js
+++ scripts/script.js
@@ -33,20 +33,20 @@ function setTheme(theme) {
 
 function setActiveButton(buttonsArray, theme) {
   buttonsArray.forEach((button) => {
-    button.classList.remove('header__theme-menu-button_active');
+    button.classList.remove('header__button_active');
     button.removeAttribute('disabled');
   });
   const target = buttonsArray.find((button) =>
     button.classList.contains(`header__theme-menu-button_type_${theme}`)
   );
   if (target) {
-    target.classList.add('header__theme-menu-button_active');
+    target.classList.add('header__button_active');
     target.setAttribute('disabled', true);
   } else {
     const autoButton = document.querySelector(
       '.header__theme-menu-button_type_auto'
     );
-    autoButton.classList.add('header__theme-menu-button_active');
+    autoButton.classList.add('header__button_active');
     autoButton.setAttribute('disabled', true);
   }
 }
